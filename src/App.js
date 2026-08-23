f2973cb6ac2fa0e356571c9883016f24fa59b5be
Veronika050987 <veronicaviv@mail.ru>
2026-08-23 15:51:33 +0300

    bug with 404 APP corrected

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import React from 'react';
 import { LanguageProvider } from './components/LanguageContext';
 import Menu from './components/Menu';
 import Home from './components/Home';
 import About from './components/About';
 import Games from './components/Games';
-import Footer from './components/Footer';
+import Footer from './components/Footer';//не забывать убирать <BrowserRouter> из index.js
 
 function App() {
   return (
     <LanguageProvider>
-    <div className="App">
-      <Menu />
-     <main>
-       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="games" element={<Games />} />
-       </Routes>
-     </main>
-     <Footer />
-    </div>
+       <HashRouter>
+        <div className="App">
+          <Menu />
+            <main>
+              <Routes> 
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/games" element={<Games />} />
+              </Routes>
+            </main>
+        <Footer />
+        </div>
+       </HashRouter>
     </LanguageProvider>
   );
 }
